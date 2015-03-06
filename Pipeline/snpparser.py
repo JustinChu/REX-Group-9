ac5f789f6165d8fd7252317af963aa5547841b7a
Kevin Chow <kevinchow97@hotmail.com>
2015-03-05 21:45:39 -0800

    added snp parser/sorter

---
+++ Pipeline/snpparser.py
@@ -0,0 +1,35 @@
+# Orders SNPs according to their allele frequency, from highest to lowest. 
+# SNPs with allele frequencies greater than the threshold are filtered out. 
+
+import re
+from collections import OrderedDict
+
+threshold = 0.95;
+countPattern = re.compile(ur'(.:\d.\d+)')
+
+f = open("outputfull.frq", "r+")
+
+result = {}
+for line in f:
+	countList = re.findall(countPattern, line)
+	if len(countList) > 0:
+		maxFreq = -1
+		for c in countList:
+			count = c.split(":", 1)
+			allele = count[0]
+			freq = float(count[1])
+			# finds the max allele frequency for each snp 
+			if freq > maxFreq:
+				maxFreq = freq
+		# filters max allele frequency 
+		if maxFreq <= threshold:
+			result[maxFreq] = line
+
+sortedResult = OrderedDict(sorted(result.items(), key=lambda r: r[0], reverse=True))
+
+for result in sortedResult.values():
+	print result
+
+	
+
+
